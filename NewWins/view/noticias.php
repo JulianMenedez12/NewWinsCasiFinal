db7308f6c682028ba5a89f9aa29f8bdedfdf0b23
JulianMenedez12 <128722932+JulianMenedez12@users.noreply.github.com>
2024-08-05 16:24:21 -0500

    Add files via upload

--- NewWins/view/noticias.php
+++ NewWins/view/noticias.php
@@ -14,11 +14,16 @@ $gestorUsuarios = new GestorUsuarios();
 $usuarios = $gestorUsuarios->listarUsuarios();
 ?>
 <head>
-    <title>Gestion de usuarios</title>
+    <title>Gestión de usuarios</title>
 </head>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
+            <?php if (isset($_GET['status']) && $_GET['status'] == 'success') : ?>
+                <div class="alert alert-success">El usuario ha sido actualizado correctamente.</div>
+            <?php elseif (isset($_GET['status']) && $_GET['status'] == 'error') : ?>
+                <div class="alert alert-danger">Hubo un error al actualizar el usuario.</div>
+            <?php endif; ?>
             <div id="usuarios" class="mt-4">
                 <h4>Usuarios Registrados</h4>
                 <div class="table-responsive mt-4">
@@ -29,6 +34,7 @@ $usuarios = $gestorUsuarios->listarUsuarios();
                                     <th>ID <br><i class='bx bx-hash'></i></th>
                                     <th>Nombre de Usuario <br> <i class='bx bxs-user'></i></th>
                                     <th>Correo Electrónico <br><i class='bx bxs-chat'></i></th>
+                                    <th>Acciones <br><i class='bx bx-cog'></i></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -37,6 +43,21 @@ $usuarios = $gestorUsuarios->listarUsuarios();
                                         <td><?php echo $usuario['id']; ?></td>
                                         <td><?php echo $usuario['nombre_usuario']; ?></td>
                                         <td><?php echo $usuario['correo_electronico']; ?></td>
+                                        <td>
+                                            <?php if ($usuario['es_admin'] == 0): ?>
+                                                <form method="post" action="../controller/cambiar_admin.php">
+                                                    <input type="hidden" name="id" value="<?php echo $usuario['id']; ?>">
+                                                    <input type="hidden" name="accion" value="dar">
+                                                    <button type="submit" class="btn btn-success">Dar Admin</button>
+                                                </form>
+                                            <?php else: ?>
+                                                <form method="post" action="../controller/cambiar_admin.php">
+                                                    <input type="hidden" name="id" value="<?php echo $usuario['id']; ?>">
+                                                    <input type="hidden" name="accion" value="quitar">
+                                                    <button type="submit" class="btn btn-danger">Quitar Admin</button>
+                                                </form>
+                                            <?php endif; ?>
+                                        </td>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
@@ -50,5 +71,4 @@ $usuarios = $gestorUsuarios->listarUsuarios();
     </div>
 </div>
 </body>
-
 </html>
